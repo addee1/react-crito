0f91799c747cdb86ae096cad794532fcddb1b0be
Adam Ottosson <addeecsgo@gmail.com>
2023-11-06 17:27:14 +0100

    upate

--- src/components/LetsConnectSection/LetsConnectSection.js
+++ src/components/LetsConnectSection/LetsConnectSection.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './LetsConnectSection.css'
+import { Link } from 'react-router-dom'
 
 import imageLine from '../../assets/images/image-lines.svg'
 
@@ -8,8 +9,8 @@ const ContactSection = () => {
     <section className="lets-connect">
         <img src={imageLine} />
         <div className="container">
-            <a href="index.html">Home</a>
-            <a href="contact.html">Contact</a>
+            <Link to="/">Home</Link>
+            <Link to="/contact">Contact</Link>
             <h1>Let's Connect</h1>
         </div>
     </section>
--- src/components/MessageUs/MessageUs.js
+++ src/components/MessageUs/MessageUs.js
@@ -1,24 +1,132 @@
-import React from 'react'
-
+import React, { useState } from 'react'
 import './MessageUs.css'
 
+
+
+
+
+
 const MessageUs = () => {
+    const [name, setName] = useState('')
+    const [nameError, setNameError] = useState('false')
+    const [email, setEmail] = useState('')
+    const [emailError, setEmailError] = useState('false')
+    const [message, setMessage] = useState('')
+    const [messageError, setMessageError] = useState('false')
+    const [errorMessage, setErrorMessage] = useState('')
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        setErrorMessage('')
+        
+        
+        
+        const sendMessage = {name, email, message}
+        const json = JSON.stringify(sendMessage)
+        
+        if (!nameError && !emailError && !messageError) {
+            const result = await fetch('https://win23-assignment.azurewebsites.net/api/contactform', {
+            method: 'post',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: json
+            })
+
+            clearForm()
+
+            switch (result.status) {
+                case 200:
+                    alert('Message Sent! Thanks for contacting us :)')
+                    break;
+                case 400:
+                    setErrorMessage('Something went wrong!')
+                    break;    
+            }
+        }
+       
+    }
+
+    const clearForm = () => {
+        setName('')
+        setEmail('')
+        setMessage('')
+    }
+
+    const handleChange = (e) => {
+        console.log(e.target.type)
+
+        switch (e.target.name) {
+            case 'name':
+                setName(e.target.value)
+                setNameError(validateLenght(e.target.value))
+                break
+            case 'email':
+                setEmail(e.target.value)
+                setEmailError(validateEmail(e.target.value))
+                break
+            case 'message':
+            setMessage(e.target.value)
+            setMessageError(validateLenght(e.target.value, 10))
+                break
+        }
+    }
+
+    const validateLenght = (value, minLenght=1) => {
+        if (value.length < minLenght)
+            return true
+        return false
+    }
+
+
+    const validateEmail = (value) => {
+        //  Måste vara en gilitg mailadress
+        const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+        return !emailPattern.test(value)
+        
+      };
+
   return (
     <section className="message-us">
         <div className="container-message-us">
             <h1>Leave us a message<br/>for any information.</h1>
-            <div className="form">
+            <form onSubmit={handleSubmit} className="form">
+                <p className="errorMessage">{errorMessage}</p>
                 <div className="input-field">
-                    <input className="name-input" type="text" name="name" placeholder="Name*" />
+                    <label className={`${nameError ? 'error': ''}`}>{`Name ${nameError ? ' is required*': ''}`}</label>
+                    <input
+                    id="nameInput"
+                    className="name-input"
+                    type="text"
+                    name="name"
+                    value={name}
+                    onChange={(e) => handleChange(e)}
+                    />
                 </div>
                 <div className="input-field">
-                    <input className="mail-input" type="text" name="mail" placeholder="Email*" />
+                    <label className={`${emailError ? 'error': ''}`}>{`Email ${emailError ? ' is not valid*': ''}`}</label>
+                    <input
+                    id="mailInput"
+                    className="mail-input"
+                    type="text"
+                    name="email"
+                    value={email}
+                    onChange={(e) => handleChange(e)}
+                    />
                 </div>
                 <div className="input-field">
-                    <input className="message-input" type="text" name="message" placeholder="Message*" />
+                    <label className={`${messageError ? 'error': ''}`}>{`Message ${messageError ? ' needs to be min. 10 characters*': ''}`}</label>
+                    <textarea
+                    id="messageInput"
+                    className="message-input"
+                    type="text"
+                    name="message"
+                    value={message}
+                    onChange={(e) => handleChange(e)}
+                    />
                 </div>
-                <button className="btn-send-message" href="#">Send Message<i className="fa-solid fa-location-arrow"></i></button>
-            </div>
+                <button type="submit" className="btn-send-message">Send Message<i className="fa-solid fa-location-arrow"></i></button>
+            </form>
         </div>
      </section>
   )
